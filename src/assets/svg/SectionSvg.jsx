b130f109c8be4bb288411a3ca1d27e5108ab93a0
NN-Bayat <noorullahbayat1414@gmail.com>
2025-03-07 21:12:14 +0430

    changes add

--- src/assets/svg/SectionSvg.jsx
+++ src/assets/svg/SectionSvg.jsx
@@ -4,13 +4,13 @@ const SectionSvg = ({ crossesOffset }) => {
   return (
     <>
       <PlusSvg
-        className={`hidden absolute top-[0.2125rem] left-[1.5625rem] ${
+        className={`hidden absolute top-[0.219rem] left-[1.5625rem] ${
           crossesOffset && crossesOffset
         } pointer-events-none lg:block xl:left-[2.1875rem]`}
       />
 
       <PlusSvg
-        className={`hidden absolute  top-[0.2125rem] right-[1.5625rem] ${
+        className={`hidden absolute  top-[0.219rem] right-[1.5625rem] ${
           crossesOffset && crossesOffset
         } pointer-events-none lg:block xl:right-[2.1875rem]`}
       />
